0b0581d40f930e6e2e390b0752d16b0f97517b82
MAlDibawy <mahmoudaldibawy@gmail.com>
2023-04-27 19:56:26 +0200

    modify api url to https instead of http

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const WEATHER_API_URL = "http://api.weatherapi.com/v1"
+export const WEATHER_API_URL = "https://api.weatherapi.com/v1"
 
 export const API_KEY = "dd84f36010294ac5950150952232102%20";
 
