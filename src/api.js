d5fb08afcfb7c6aac4fd549001b26929c1e5d45b
MAlDibawy <mahmoudaldibawy@gmail.com>
2023-04-27 17:09:13 +0200

    add {searchInput, currentWeather} components, implement search and fetch data from api + some design changes

---
+++ src/api.js
@@ -0,0 +1,4 @@
+export const WEATHER_API_URL = "http://api.weatherapi.com/v1"
+
+export const API_KEY = "dd84f36010294ac5950150952232102%20";
+
--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { WEATHER_API_URL, API_KEY } from "../API";
 
 export default function SearchInput() {
-  return <div>SearchInput</div>;
+  const [inputText, setInputText] = useState("");
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecastWeather, setForecastWeather] = useState(null);
+
+  const getWeatherData = (inputText) => {
+    axios
+      .get(`${WEATHER_API_URL}/current.json?key=${API_KEY}&q=${inputText}`)
+      .then((response) => setCurrentWeather(response.data))
+      .catch((err) => console.log(err));
+    axios
+      .get(
+        `${WEATHER_API_URL}/forecast.json?key=${API_KEY}&q=${inputText}&days=3`
+      )
+      .then((response) => setForecastWeather(response.data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    if (inputText.trim() !== "") {
+      getWeatherData(inputText);
+    }
+  }, [inputText]);
+
+  return (
+    <>
+      <div className="container">
+        <input
+          className="form-control searchInput"
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Enter City ..."
+          onChange={(e) => setInputText(e.target.value)}
+        />
+      </div>
+    </>
+  );
 }
