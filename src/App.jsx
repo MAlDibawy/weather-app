8a71532ed5d75809abc337026d804ac4025427be
MAlDibawy <mahmoudaldibawy@gmail.com>
2023-07-11 03:44:29 +0300

    Enhance Style

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
   return (
     <>
       <div className="container py-2">
+        <h1 className="header">Weather Forecast</h1>
+        <h3 className="header mb-0">Know Before You Go !</h3>
         <SearchInput />
       </div>
     </>
